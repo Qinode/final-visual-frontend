d94370048915a669633ab17eec16fae89e15ef14
Zheng Qin <zheng.qin29@outlook.com>
2018-06-19 23:44:06 +0100

    change password

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -1,29 +1,28 @@
 import router from "../../router";
 
 const state = {
-    isAuth: false,
-    onFail: false
+    isAuth: false
 };
 
 // getters
 const getters = {
-    isAuth: state => {
+    isAuth: (state) => {
         return state.isAuth;
-    }
+    },
 };
 
 // mutations
 const mutations = {
     login(state, payload) {
         state.isAuth = true;
-    }
+    },
 };
 
 // actions
 const actions = {
     login({ commit, state }, payload) {
         if (!state.isAuth) {
-            if (payload.password === "psd") {
+            if (payload.password === "ridgeviewAESE") {
                 commit("login", payload);
                 router.push("/map");
             }
